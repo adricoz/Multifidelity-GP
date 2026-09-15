918e94402625ea6e28795ac9b44de161b032745f
adricoz <adrian@banulsdesign.com>
2026-09-15 16:19:18 +0200

    modified:   Kernel.py

--- Kernel.py
+++ Kernel.py
@@ -1,1 +1,21 @@
 # this file is all about defining the kernel (covariance function) used for gaussian processes
+import numpy as np
+from non-nested.non_nested_mf_covariance import base_covariance_matrix
+
+class Kernel:
+    def __init__(self, lengthscale, variance):
+        self.lengthscale = lengthscale
+        self.variance = variance
+
+    def compute_covariance_matrix(self, X):
+        """
+        Computes the covariance matrix for the dataset X using the specified kernel.
+        
+        Arguments:
+        - X: numpy array of shape (n, d), the input dataset.
+        
+        Returns:
+        - K: numpy array of shape (n, n), the covariance matrix.
+        """
+        Theta_l = np.concatenate((self.lengthscale, [self.variance]))
+        return base_covariance_matrix(X, Theta_l)
